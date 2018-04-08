adbcc0fb8c07d2c1a019c3935a1e916d555b7fb3
Your Name <you@example.com>
2018-04-08 15:15:51 +0400

    lesson2

---
+++ public/xot.js
@@ -0,0 +1,16 @@
+class Grass extends Livingcreature{
+    
+
+    mul() {
+        this.multiply++;
+        this.direction = random(this.yntrelVandak(0));
+        if (this.multiply >= this.speed && this.direction) {
+            var newGrass = new Grass(this.direction[0], this.direction[1], this.index);
+            newGrass.parentX = this.x;
+            newGrass.parentY = this.y;
+            grassArr.push(newGrass);
+            matrix[this.direction[1]][this.direction[0]] = this.index;
+            this.multiply = 0;
+        }
+    }
+}
